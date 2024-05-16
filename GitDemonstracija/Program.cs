dfd342bb13d613bba6b138286871aa9215c57581
Sarunas <sarugen12@gmail.com>
2024-05-16 13:49:44 +0200

    Daug extra eiluciu

--- GitDemonstracija/Program.cs
+++ GitDemonstracija/Program.cs
@@ -1,3 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Goodbye, World!");
 Console.WriteLine("Goodbye, World!");
+Console.WriteLine("Goodbye, World!");
+Console.WriteLine("Goodbye, World!");
+Console.WriteLine("Goodbye, World!");
+Console.WriteLine("Goodbye, World!");
+Console.WriteLine("Goodbye, World!");
+Console.WriteLine("Goodbye, World!");
+Console.WriteLine("Goodbye, World!");
+Console.WriteLine("Goodbye, World!");
+Console.WriteLine("Goodbye, World!");
+
+
